b2af0f7f6c9ff57d5f6017f5ea1a06598e5b3561
Sumit Satre <sumitsatre03@gmail.com>
2025-03-17 10:23:06 +0530

    second commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var server = http.createServer(function (req, res) {
 
         req.on('end', function() {
             if (req.url === '/') {
-                log('Received message: ' + body);
+                log('Received message is this : ' + body);
             } else if (req.url = '/scheduled') {
                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
